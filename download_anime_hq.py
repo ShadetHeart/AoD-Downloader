d9d32cae175697cfd7a091b41fa87042ae949383
YugiFanGX <12579036+YugiFanGX@users.noreply.github.com>
2020-07-18 13:26:04 +0000

    Shebang

--- download_anime_hq.py
+++ download_anime_hq.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import requests
 import os
 import re
@@ -54,7 +56,7 @@ def download(url,title,episode):
 			os.remove(filename)
 			break
 			
-		os.system('cls')
+		os.system('clear')
 		print("Downloading Episode " + str(counter) + ":")
 		print("Downloaded chunk " + str(num+1) + " of " +  str(num_max+1) + " (" + percent + "%)")
 			
@@ -82,7 +84,7 @@ def download(url,title,episode):
 		
 	print("Done")
 
-os.system('cls')
+os.system('clear')
 	
 #asking for login info
 username = input("Enter username: ")
