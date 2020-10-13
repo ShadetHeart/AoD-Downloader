36c5ebf3d12abdcab37dee9a790dd43eb1dce81c
h4llow3En <development@felixdoering.com>
2020-10-13 13:14:00 +0200

    Remove unneeded imports

--- AoD_downloader.py
+++ AoD_downloader.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 import requests
-import os
 import re
-import shutil
 import configparser
 import json
-import sys
-import pprint
 from bs4 import BeautifulSoup
 from ffmpeg import FFmpeg
 
